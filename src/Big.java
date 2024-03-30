f7e537583ac10dbcea787354b5458733fc125d90
Psycotik <sgtpsycotik@gmail.com>
2024-03-30 01:40:24 -0700

    Answered questions in README.md. Completed Big.java.

--- src/Big.java
+++ src/Big.java
@@ -1,5 +1,24 @@
+import java.math.BigInteger;
+
 public class Big {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        displayFactorialTable(999999);
+    }
+
+    public static void displayFactorialTable(int n) {
+        System.out.println("Integer\tFactorial");
+        for (int i = 0; i <= n; i++) {
+            BigInteger factorial = calculateFactorial(BigInteger.valueOf(i));
+            System.out.println(i + "\t" + factorial);
+        }
+    }
+
+    public static BigInteger calculateFactorial(BigInteger num) {
+        BigInteger factorial = BigInteger.ONE;
+        for (BigInteger i = BigInteger.ONE; i.compareTo(num) <= 0; i = i.add(BigInteger.ONE)) {
+            factorial = factorial.multiply(i);
+        }
+        return factorial;
     }
 }
